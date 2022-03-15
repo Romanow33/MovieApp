438d155a5467afc68719832eaf017d5a9d668e9e
Ignacio Romanow <romanowignacio1@gmail.com>
2022-03-15 10:51:44 -0300

    Add react-scroll

--- src/components/moviesGrid.jsx
+++ src/components/moviesGrid.jsx
@@ -5,11 +5,22 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 import { Empty } from "./Empty";
 import { useMovies } from "../hooks/useMovies";
+import { animateScroll as scroll } from "react-scroll";
 export function MoviesGrid({ search }) {
   const { movies, isLoading, hasNextPage, fetchNextPage } = useMovies(search);
   if (!isLoading && movies.length === 0) {
     return <Empty />;
   }
+
+  const onClickDown = () => {
+    scroll.scrollMore(650);
+  };
+  const onClickUp = () => {
+    scroll.scrollMore(-650);
+  };
+  const onClickHome = () => {
+    scroll.scrollToTop();
+  };
 
   return (
     <InfiniteScroll
@@ -19,6 +30,15 @@ export function MoviesGrid({ search }) {
       next={() => fetchNextPage()}
       loader={<Spiner />}
     >
+      <button className={styles.buttonScrollUp} onClick={onClickUp}>
+        up
+      </button>
+      <button className={styles.buttonScrolldown} onClick={onClickDown}>
+        down
+      </button>
+      <button className={styles.toHome} onClick={onClickHome}>
+        HOME
+      </button>
       <ul className={styles.moviesGrid}>
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
